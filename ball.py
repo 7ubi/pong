4de649d259624a8e77096c80e62fd2dd52215fef
Tobi <51411393+7ubi@users.noreply.github.com>
2020-06-06 13:22:24 +0200

    basically works

--- ball.py
+++ ball.py
@@ -1,11 +1,28 @@
 import pygame
+import math
 
-class ball:
-	def __init__(self, x, y, w, h, speed, angle, dir):
+
+class Ball:
+	def __init__(self, x, y, r, speed, angle, dir):
 		self.x = x
 		self.y = y
-		self.w = w
-		self.h = h
+		self.r = r
 		self.speed = speed
 		self.angle = angle
 		self.dir = dir
+
+	def update(self):
+		self.x += math.cos(self.angle * -self.dir) * self.dir * self.speed
+		self.y += math.sin(self.angle * -self.dir) * self.dir * self.speed
+
+		self.x = int(self.x)
+		self.y = int(self.y)
+
+	def changeAngle(self, angle):
+		self.angle = angle
+
+	def changeDir(self):
+		self.dir *= -1
+
+	def show(self, screen):
+		pygame.draw.circle(screen, pygame.Color(255, 255, 255), (int(self.x), int(self.y)), self.r)
--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame
+import math
+import numpy as np
 from paddle import Paddle
+from ball import Ball
 
 pygame.init()
 
@@ -10,11 +13,13 @@ pygame.display.set_caption("PONG")
 
 clock = pygame.time.Clock()
 
-left = Paddle(95, 200, 10, 75, 3)
-right = Paddle(495, 200, 10, 75, 3)
 
 
 def gameLoop():
+	left = Paddle(95, 200, 10, 75, 5)
+	right = Paddle(495, 200, 10, 75, 5)
+
+	ball = Ball(width/2, height/2, 10, 4, 0, 1)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -36,8 +41,49 @@ def gameLoop():
 
 
 		screen.fill(pygame.Color(0, 0, 0))
+
 		left.show(screen)
 		right.show(screen)
+
+		if ball.x < left.x + left.w + ball.r and ball.x > left.x - ball.r and ball.dir == -1  or ball.x > right.x - ball.r and ball.x < right.x + right.w + ball.r and ball.dir == 1:
+			if ball.y > left.y and ball.y <= left.y + left.h / 8 or ball.y > right.y and ball.y <= right.y + right.h / 8:
+				ball.changeDir()
+				ball.changeAngle(np.pi/4)
+
+			if ball.y > left.y + left.h / 8 and ball.y <= left.y + left.h / 8 * 2 or ball.y > right.y + right.h/8 and ball.y <= right.y + right.h / 8 * 2:
+				ball.changeDir()
+				ball.changeAngle(30 * np.pi/180)
+
+			if ball.y > left.y + left.h / 8 * 2 and ball.y <= left.y + left.h / 8 * 3 or ball.y > right.y + right.h/8 * 2 and ball.y <= right.y + right.h / 8 * 3:
+				ball.changeDir()
+				ball.changeAngle(15 * np.pi/180)
+
+			if ball.y > left.y + left.h / 8 * 3 and ball.y <= left.y + left.h / 8 * 5 or ball.y > right.y + right.h/8 * 3 and ball.y <= right.y + right.h / 8 * 5:
+				ball.changeDir()
+				ball.changeAngle(0)
+
+			if ball.y > left.y + left.h / 8 * 5 and ball.y <= left.y + left.h / 8 * 6 or ball.y > right.y + right.h/8 * 5 and ball.y <= right.y + right.h / 8 * 6:
+				ball.changeDir()
+				ball.changeAngle(-15 * np.pi/180)
+
+			if ball.y > left.y + left.h / 8 * 6 and ball.y <= left.y + left.h / 8 * 7 or ball.y > right.y + right.h/8 * 6 and ball.y <= right.y + right.h / 8 * 7:
+				ball.changeDir()
+				ball.changeAngle(-30 * np.pi/180)
+
+			if ball.y > left.y + left.h / 8 * 7 and ball.y <= left.y + left.h or ball.y > right.y + right.h / 8 * 7 and ball.y <= right.y + right.h:
+				ball.changeDir()
+				ball.changeAngle(-np.pi/4)
+
+			
+		if ball.y > height - ball.r or ball.y < ball.r:
+			ball.changeAngle(-ball.angle)
+			
+		if ball.x < 0 or ball.x > width:
+			gameLoop()
+			return	
+
+		ball.update()
+		ball.show(screen)
 		pygame.display.update()
 		clock.tick(60)
 
